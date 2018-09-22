824ff75e8fe80ff72d1fa490b63667ab30ee5b9f
Cory Waters <csw@Corys-MacBook-Pro.local>
2018-09-22 12:15:36 -0400

    added script to combine seperate files

---
+++ create_master_file.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+
+yh <- read_csv('processed_data/yahoo_nfl_2016_2017.csv',
+               col_types = 
+                 cols(gid = col_character()))
+yh <- mutate(yh, platform = 'yh')
+
+dk <- read_csv('processed_data/draftkings_nfl_2014_2017.csv',
+               col_types = 
+                 cols(gid = col_character()))
+dk <- mutate(dk, platform = 'dk')
+
+fd <- read_csv('processed_data/fanduel_nfl_2011_2017.csv', 
+               col_types = 
+                 cols(gid = col_character()))
+fd <- mutate(fd, platform = 'fd')
+
+yh %>% 
+  bind_rows(dk) %>% 
+  bind_rows(fd) %>% 
+  arrange(platform, desc(year), week) %>% 
+  write_csv('processed_data/nfl_pts_history.csv')
+  
+
+
+
---
+++ download_nfl_pts.R
@@ -0,0 +1,48 @@
+library(rvest)
+library(tidyverse)
+
+get_pts_data <- function(df){
+  pts_data <- df %>% 
+    html_nodes('pre') %>% 
+    html_text() %>% 
+    read_delim(delim = ';') %>% 
+    set_names(c('week', 'year', 'gid', 'name', 'pos', 'team', 'location', 'opp', 'pts', 'salary'))
+  
+  # Be nice to the site
+  Sys.sleep(sample(seq(0,2,0.5), 1))
+  
+  pts_data %>% 
+    mutate(gid = as.character(gid))
+}
+
+make_urls <- function(base_url, weeks, years, league){
+  tmp <- expand.grid(base_url, weeks,'&year=', years,'&game=',league,'&scsv=1')
+  paste0(tmp$Var1, tmp$Var2, tmp$Var3, tmp$Var4, tmp$Var5, tmp$Var6, tmp$Var7)
+}
+
+# Example URL below
+# http://rotoguru1.com/cgi-bin/fyday.pl?week=17&year=2017&game=fd&scsv=1
+base_url <- 'http://rotoguru1.com/cgi-bin/fyday.pl?week='
+weeks <- 1:17 # only covers regular season
+
+# fanduel goes back to 2011, draft kings to 2014, yahoo to 2016
+# fd = fanduel, dk = draft kings, yh = yahoo
+fd_urls <- make_urls(base_url, weeks, 2011:2017, 'fd')
+dk_urls <- make_urls(base_url, weeks, 2014:2017, 'dk')
+yh_urls <- make_urls(base_url, weeks, 2016:2017, 'yh')
+
+fanduel_data <- map(fd_urls, ~read_html(.x) %>% get_pts_data())
+draftkings_data <- map(dk_urls, ~read_html(.x) %>% get_pts_data())
+yahoo_data <- map(yh_urls, ~read_html(.x) %>% get_pts_data())
+
+fanduel_data %>% 
+  bind_rows() %>% 
+  write_csv('processed_data/fanduel_nfl_2011_2017.csv')
+
+draftkings_data %>% 
+  bind_rows() %>% 
+  write_csv('processed_data/draftkings_nfl_2014_2017.csv')
+
+yahoo_data %>% 
+  bind_rows() %>% 
+  write_csv('processed_data/yahoo_nfl_2016_2017.csv')
