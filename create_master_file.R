3fe072fb1790664633e222b3aeb1de636d9a8bc6
Cory Waters <csw@Corys-MacBook-Pro.local>
2019-07-20 16:31:34 -0400

    2018 updates

--- create_master_file.R
+++ create_master_file.R
@@ -1,16 +1,16 @@
 library(tidyverse)
 
-yh <- read_csv('processed_data/yahoo_nfl_2016_2017.csv',
+yh <- read_csv('processed_data/yahoo_nfl_2016_2018.csv',
                col_types = 
                  cols(gid = col_character()))
 yh <- mutate(yh, platform = 'yh')
 
-dk <- read_csv('processed_data/draftkings_nfl_2014_2017.csv',
+dk <- read_csv('processed_data/draftkings_nfl_2014_2018.csv',
                col_types = 
                  cols(gid = col_character()))
 dk <- mutate(dk, platform = 'dk')
 
-fd <- read_csv('processed_data/fanduel_nfl_2011_2017.csv', 
+fd <- read_csv('processed_data/fanduel_nfl_2011_2018.csv', 
                col_types = 
                  cols(gid = col_character()))
 fd <- mutate(fd, platform = 'fd')
